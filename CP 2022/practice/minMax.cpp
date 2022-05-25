97104d4a1e3f67da81fb13cd23a01d3ae88304f4
mrifat1 <44732773+mrifat1@users.noreply.github.com>
2022-05-26 00:12:54 +0600

    efficient minMax in c++

---
+++ CP 2022/practice/minMax.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,t,i,j,arr[10000],min=INT_MAX,max=INT_MIN ;
+    cin>>n;
+    // cout<<LONG_LONG_MAX<<"\n";
+    for ( i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        if(arr[i]<min)
+        {
+            min = arr[i];
+        }
+        else if(arr[i]>max)
+         {
+             max = arr[i];
+         }
+        // else
+        //  min = arr[i];
+        /* code */
+    }
+    cout<<"Maximum "<<max<<"\n";
+    cout<<"Minimum "<<min<<"\n";
+    
+
+
+}
