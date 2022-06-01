250caf094aef244c1eab91ba68ec47c7c837ceba
mrifat1 <44732773+mrifat1@users.noreply.github.com>
2022-06-02 01:13:47 +0600

    A simple C++ program to check for even or odd without using if-else

---
+++ CP 2022/DSA/rightShift.cpp
@@ -0,0 +1,12 @@
+#// A simple C++ program to check for
+// even or odd without using if-else
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int cal;
+    cal = (n+1)/2;
+    cout<<cal*2<<endl;
+}
