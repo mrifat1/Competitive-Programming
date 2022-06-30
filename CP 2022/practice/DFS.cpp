311aa79e605c973962744fcae6be46a041265b35
mrifat1 <44732773+mrifat1@users.noreply.github.com>
2022-06-30 17:45:39 +0600

    dfs

--- CP 2022/practice/DFS.cpp
+++ CP 2022/practice/DFS.cpp
@@ -5,21 +5,21 @@ int vis[105];
 // int f[105];
 int ar[105][105];
 int cnt,d[105],f[105];
+int sizeOfV[105];
 // vector<int>v[105];
-void dfs(int node)
+void dfs(int node, int sz)
 {
     vis[node]=1;
-    d[node] = cnt++;
-    int sz = sizeof(ar[0]);
-    cout<<"size "<<sz<<endl;
+    d[node] = ++cnt;
+    // cout<<"size "<<sz<<endl;
     for(int i = 0; i < sz; i++)
     {
         if(!vis[ar[node][i]])
         {
-            dfs(ar[node][i]);
+            dfs(ar[node][i],sizeOfV[ar[node][i]]);
         }
     }
-    f[node] = cnt++;
+    f[node] = ++cnt;
 
 }
 int main()
@@ -31,6 +31,7 @@ int main()
         /* code */
         cin>>u>>vi;
         int k;
+        sizeOfV[u] = vi;
         for (int j = 0; j < vi; j++)
         {
             /* code */
@@ -40,7 +41,7 @@ int main()
     for(int i = 1 ; i <= n; i++)
     {
         if(!vis[i])
-            dfs(i);
+            dfs(i,sizeOfV[i]);
     }
     for(int i = 1 ; i <= n; i++)
     {
