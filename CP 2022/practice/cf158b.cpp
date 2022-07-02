54c872d7f75b316dda7dff9d192d57db178560cf
mrifat1 <44732773+mrifat1@users.noreply.github.com>
2022-07-03 03:28:15 +0600

    DSA practice

---
+++ CP 2022/practice/cf158b.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum;
+int cou[5];
+int main()
+{
+    long long t,ar[1000010];
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        cin>>ar[i];
+        // sum+=ar[i];
+        cou[ar[i]]++;
+    }
+    int ans = cou[4]+cou[3]+cou[2]/2;
+    if(cou[1] > cou[3])
+    {
+        cou[1] -= cou[3];
+    }
+    else{
+        cou[1]=0;
+    }
+    if(cou[2] % 2 == 1)
+    {
+        ans +=1;
+        cou[1] -= 2;
+    }
+    if(cou[1]>0)
+    {
+        if(cou[1] % 4)
+        {
+            ans += (cou[1]/4)+1;
+        }
+        else
+        {
+            ans += cou[1]/4;
+        }
+    }
+    cout<<ans<<endl;
+
+}
