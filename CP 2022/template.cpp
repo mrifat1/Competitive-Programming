3325157da0d0aa35bf685cae5e4ba318dc31e4e3
Faisal Mahmud <faisalmahmud@Faisals-MacBook-Pro.local>
2022-09-15 01:50:33 +0600

    new setup and demo solve

--- CP 2022/template.cpp
+++ CP 2022/template.cpp
@@ -1,7 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
      ios_base::sync_with_stdio(0);
      cin.tie(0);
+     int n;
+     cin >> n;
+     cout << n << endl;
 }
